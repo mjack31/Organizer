60e463d70438498291e95eb0c93906e3e7a147ba
mjack31 <shark3113@gmail.com>
2018-12-03 00:20:02 +0100

    Add commets

--- Organizer.UI/ViewModels/FriendDetailsViewModel.cs
+++ Organizer.UI/ViewModels/FriendDetailsViewModel.cs
@@ -16,23 +16,30 @@ namespace Organizer.UI.ViewModels
         private IFriendDetailsDataService _friendDataService;
         private IEventAggregator _eventAggregator;
 
+        // do konstruktora przekazujemy wszystkie obiekty na których chcemy pracować - IoC
         public FriendDetailsViewModel(IFriendDetailsDataService friendDataService, IEventAggregator eventAggregator)
         {
             _friendDataService = friendDataService;
             _eventAggregator = eventAggregator;
+
+            // dodanie subskrybenta (handlera) do eventu EventAggregatora Prism'a. Metoda Subscribe przyjmuje Action<T>
+            // ale można zamiast delegaty dodawać normalnie metody - ułatwienie
             _eventAggregator.GetEvent<ListItemChosenEvent>().Subscribe(_onListItemChosen);
         }
 
+        // event handler wybrania danego Friendsa
         private async void _onListItemChosen(int id)
         {
             await LoadFriendAsync(id);
         }
 
+        // ładowanie pełnych danych wybranego przyjaciela
         public async Task LoadFriendAsync(int friendId)
         {
             Friend = await _friendDataService.GetFriendAsync(friendId);
         }
 
+        // propery do którego zbindowany jest widok
         public Friend Friend
         {
             get { return _friend; }
@@ -40,6 +47,7 @@ namespace Organizer.UI.ViewModels
             {
                 _friend = value;
                 // przy każdym setowaniu odpalać ten event aby UI się updateowało
+                // tutaj UI musi wiedzieć że został wybrany inny Friend i zupdateować View na tej podstawie
                 OnProperyChanged(nameof(Friend));
             }
         }
--- Organizer.UI/ViewModels/FriendsListViewModel.cs
+++ Organizer.UI/ViewModels/FriendsListViewModel.cs
@@ -16,15 +16,20 @@ namespace Organizer.UI.ViewModels
         private IFriendsDataService _friendsDataService;
         private IEventAggregator _eventAggregator;
 
+        // do konstruktora przekazujemy wszystkie obiekty na których chcemy pracować - IoC
         public FriendsListViewModel(IFriendsDataService friendsDataService, IEventAggregator eventAggregator)
         {
+            // unikać instancjowania obiektów przez new, chyba że sąto listy, słowniki itp
             FriendsList = new ObservableCollection<ListItem>();
             _friendsDataService = friendsDataService;
             _eventAggregator = eventAggregator;
         }
 
+        // Aktywna lista która updateuje UI gdy zmienia się jej zawartość
+        // Do tego propery zbindowany jest ListView
         public ObservableCollection<ListItem> FriendsList { get; set; }
 
+        // ładowanie listy przyjaciół
         public async Task LoadDataAsync()
         {
             var listItems = await _friendsDataService.GetAllAsync();
@@ -35,8 +40,8 @@ namespace Organizer.UI.ViewModels
             }
         }
 
+        // property do którego zbindowanne jest SelectedItem elementu ListView widoku
         private ListItem _selectedFriend;
-
         public ListItem SelectedFriend
         {
             get { return _selectedFriend; }
@@ -44,7 +49,10 @@ namespace Organizer.UI.ViewModels
             {
                 _selectedFriend = value;
                 // przy każdym setowaniu odpalać ten event aby UI się updateowało
-                OnProperyChanged(nameof(SelectedFriend));
+                // tutaj chyba nie musi być UI powiadamiane o zmianie
+                // OnProperyChanged(nameof(SelectedFriend));
+
+                // odpalenie event z EventAggregatora Prism'a. Klasa ListItemChosenEvent definiuje że payload jest typu int
                 _eventAggregator.GetEvent<ListItemChosenEvent>().Publish(SelectedFriend.Id);
             }
         }
